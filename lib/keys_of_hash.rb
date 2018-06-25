311bf4eab39c872b0d9581d8d4f34bd2e63860cd
Dan Chung <danetix@gmail.com>
2018-06-25 17:55:42 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    my_array = Array.new
+
+    arguments.each do |x|
+      results = self.select { |k, v| v == x }
+      my_array << results.keys
+    end
+
+    return my_array.flatten
   end
 end
